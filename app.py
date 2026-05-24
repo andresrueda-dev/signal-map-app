e420a56ddf2e6b00da1dbfb798097be940f6f943
Andrés Emmanuel Rueda Alor <272964007+andresrueda-dev@users.noreply.github.com>
2026-05-24 01:43:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ from PIL import Image
 from streamlit_option_menu import option_menu
 
 st.set_page_config(
+    page_title="SignalMap AI",
+    page_icon="⚡",
+    layout="wide",
+    initial_sidebar_state="collapsed"
+)
+st.set_page_config(
 page_title="SignalMap AI 🌌",
 page_icon="🌌",
 layout="wide",
